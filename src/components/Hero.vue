b007e64ac2f355cffcedf04bf429aa17bbb162cd
Robin <robin.pahlman@gmail.com>
2022-02-05 22:42:35 -0500

    adjust line height

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,8 +3,8 @@
   <!-- <div class="bg-hero bg-no-repeat bg-cover flex flex-col justify-center w-full h-screen"> -->
     <h1 class="font-rubik-mono tracking-tighter text-7xl font-black text-white text-left leading-[1rem] justify-self-start col-span-3">ELECTRIC SAUNA<br>PRODUCTIONS</h1>
     <p class="font-catamaran text-1xl text-white text-left p-1 align-self-start justify-self-end">WE MAKE PRODUCTION MUSIC FOR<br>MUSIC LIBRARIES.<br>WE WORK IN VARIOUS STYLES OF<br><span class="text-red-500">ROCK, POP, AND ACOUSTIC MUSIC.</span></p>
-    <h2 class="font-catamaran text-xxl font-black text-red-500 align-self-start">MUSIC</h2>
-    <p class="text-xxl font-rubik-mono text-white align-self-start">V</p>
+    <h2 class="font-catamaran text-xxl font-black text-red-500 align-self-start leading-10">MUSIC</h2>
+    <p class="text-6xl font-rubik-mono text-white align-self-start leading-10">V</p>
     <h2 class="font-rubik-mono tracking-8 text-white text-left text-7xl leading-[0.5rem] font-black col-span-3 justify-self-center align-self-start">FOR TV<br>AND FILM</h2>
     <!-- <h2 class="font-rubik-mono tracking-tighter text-white text-8xl leading-[0.5rem] font-black col-span-3 justify-self-center">AND FILM</h2> -->
   </div>
